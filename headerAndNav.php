bca427506d11b5d505aa423497efe4ce239ad074
Srihari Rao <harirao3@gmail.com>
2014-06-22 18:54:51 -0400

    modularizing php

---
+++ headerAndNav.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kingHenry
+ * Date: 6/22/14
+ * Time: 6:51 PM
+ */
+?>
+
+<!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-fixed-top.css" rel="stylesheet">
+
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!-- Fixed navbar -->
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">phpBench</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#contact">Contact</a></li>
+
+            </ul>
+
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+<div class="container">
+    <div class="well col-md-3 col-lg-3 col-sm-3">
+        <a href="index.php">Top 10 Current Employees</a></br>
+        <a href="query2.php">Customer Service Managers</a></br>
+        <a href="query3.php">Average Salary by Department</a></br>
+        <a href="query4.php">Number of Employees by Department </a></br>
+
+
+
+    </div>
--- query2.php
+++ query2.php
@@ -12,55 +12,9 @@
 
     <title>Customer Service Managers | phpBench</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/navbar-fixed-top.css" rel="stylesheet">
-
-
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-<!-- Fixed navbar -->
-<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">phpBench</a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="#contact">Contact</a></li>
-
-            </ul>
-
-        </div><!--/.nav-collapse -->
-    </div>
-</div>
-
-<div class="container">
-    <div class="well col-md-3 col-lg-3 col-sm-3">
-        <a href="index.php">Top 10 Current Employees</a></br>
-        <a href="query2.php">Customer Service Managers</a></br>
-        <a href="query3.php">Average Salary by Department</a></br>
-        <a href="query4.php">Number of Employees by Department </a></br>
-
-
-
-    </div>
+<?php
+    @include('headerAndNav.php');
+?>
 
     <div class="col-md-9 col-lg-9 col-sm-9">
 
