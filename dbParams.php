8984b60f5a320bd3311b8aaedb0da21bebd18da6
Srihari Rao <harirao3@gmail.com>
2014-06-22 18:45:15 -0400

    modularizing php

---
+++ dbParams.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kingHenry
+ * Date: 6/22/14
+ * Time: 6:39 PM
+ */
+
+
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql dbName ***/
+$dbName='employees';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = 'lDSSJ1Sp2y';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
--- query2.php
+++ query2.php
@@ -6,28 +6,9 @@
  * Time: 4:12 PM
 
 /*** mysql hostname ***/
-$hostname = 'localhost';
 
-/*** mysql dbName ***/
-$dbName='employees';
-
-/*** mysql username ***/
-$username = 'root';
-
-/*** mysql password ***/
-$password = 'lDSSJ1Sp2y';
-
+    @require('dbParams.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
     <title>Customer Service Managers | phpBench</title>
 
