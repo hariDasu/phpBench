e904c0a83fb79ac2b7eda67c68427e90965f6767
Srihari Rao <harirao3@gmail.com>
2014-06-22 14:17:55 -0400

    first commit, testing pdo

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kingHenry
+ * Date: 6/22/14
+ * Time: 2:06 PM
+ */
+
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql dbName ***/
+$dbName='EMPLOYEES';
+
+/*** mysql username ***/
+$username = 'ROOT';
+
+/*** mysql password ***/
+$password = 'lDSSJ1Sp2y';
+
+try{
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbName",$username,$password);
+    echo 'Connected to database';
+    }
+    catch(PDOException $e)
+    {
+    echo $e->getMessage();
+    }
+
+
+
+?>
